07525b87faef184f3e7a653f58aa8791900df38c
Farhan Mufti Hilmy <farhan@ainosi.co.id>
2022-10-04 08:20:26 +0700

    Instantiate ViewModels

--- app/src/main/java/com/rechit/rituals/ui/random/RandomRitualActivity.kt
+++ app/src/main/java/com/rechit/rituals/ui/random/RandomRitualActivity.kt
@@ -2,11 +2,19 @@ package com.rechit.rituals.ui.random
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.rechit.rituals.R
+import com.rechit.rituals.ui.ViewModelFactory
 
 class RandomRitualActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: RandomRitualViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ritual_list)
+
+        val factory = ViewModelFactory.getInstance(this)
+        viewModel = ViewModelProvider(this, factory).get(RandomRitualViewModel::class.java)
     }
 }
