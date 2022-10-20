27a1c1ee581cd27e9dba11687d509cbbc7438730
StanceFreak <jombor.army67@gmail.com>
2022-10-20 12:19:33 +0800

    Added viewbinding, setup recycler, and button intent

--- app/src/main/java/com/rechit/rituals/ui/list/RitualListActivity.kt
+++ app/src/main/java/com/rechit/rituals/ui/list/RitualListActivity.kt
@@ -1,27 +1,53 @@
 package com.rechit.rituals.ui.list
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.rechit.rituals.R
+import com.rechit.rituals.databinding.ActivityRitualListBinding
 import com.rechit.rituals.ui.ViewModelFactory
+import com.rechit.rituals.ui.add.AddRitualActivity
 import com.rechit.rituals.ui.random.RandomRitualViewModel
 
-private lateinit var recycler: RecyclerView
-private lateinit var viewModel: RitualListViewModel
-
 class RitualListActivity : AppCompatActivity() {
-
+    private lateinit var recycler: RecyclerView
     private lateinit var viewModel: RitualListViewModel
+    private val binding by lazy { ActivityRitualListBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ritual_list)
-        setSupportActionBar(findViewById(R.id.toolbar))
+
+        installSplashScreen()
+        setupRecycler()
+
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         val factory = ViewModelFactory.getInstance(this)
         viewModel = ViewModelProvider(this, factory).get(RitualListViewModel::class.java)
 
+        binding.fab.setOnClickListener {
+            val i = Intent(this, AddRitualActivity::class.java)
+            startActivity(i)
+        }
+    }
+
+    fun setupRecycler() {
+        val layoutContent = binding.layoutContent
+        val recyclerRitual = layoutContent.rvRitual
+        val ritualAdapter = RitualAdapter()
+        recyclerRitual.apply {
+            layoutManager = LinearLayoutManager(
+                this@RitualListActivity,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+            adapter = ritualAdapter
+            setHasFixedSize(true)
+        }
     }
 }
